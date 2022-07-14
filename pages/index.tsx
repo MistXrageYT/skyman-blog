861e797e5e1c20db1dd882be18df37094e1edd09
SohanRajput121 <98887020+SohanRajput121@users.noreply.github.com>
2022-07-14 18:57:00 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home({ posts }: Props) {
 
               <div className='border rounded-lg group cursor-pointer overflow-hidden'>
 
-                <img className='h-60 w-full object-cover group-hover:scale-105 transition-transfrom duration-200 ease-in-out' src={urlFor(post.mainImage).url()!} alt="" />
+                <img className='h-60 w-full group-hover:scale-105 transition-transfrom duration-200 ease-in-out' src={urlFor(post.mainImage).url()!} alt="" />
                 
               <div className='flex justify-between p-5 bg-white'>
 
